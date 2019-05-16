1e9529690cefecb1afb4e831b70afb64ce6ef3ea
github username <github email address>
2019-05-15 22:43:28 -0500

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,14 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(array)
+  counter = 1
+  
+  while counter < array.length
+    yield counter
+    counter += 1
+  end
+  
+  array
+end
+
+my_each(array) do |i|
+  puts array[i]
 end
