b92acacbf5e08f2f2f1100b4c4f8a3659f15904a
github username <github email address>
2019-05-16 10:53:47 -0500

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,14 +1,15 @@
 def my_each(array)
-  counter = 1
+  counter = 0
   
   while counter < array.length
-    yield counter
+    yield array[counter]
     counter += 1
   end
   
   array
 end
 
-my_each(array) do |i|
-  puts array[i]
-end
+# my_each(array) do |i|
+#   variable = []
+#   variable.push(array[i])
+# end
